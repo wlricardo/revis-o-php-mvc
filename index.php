984a0ea41be294dd724b742b7a7e1169eb6f92f4
wlricardo <wlricardo@gmail.com>
2026-08-12 22:26:19 -0300

    Adicionando arquivos de css e imagens

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once('config.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prontuário Veterinário</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles/styles.css">
 </head>
 
 <body>
