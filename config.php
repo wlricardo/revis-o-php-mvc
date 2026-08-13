e8bfeb51a8c98212a3ed2823d30a394633db0c8c
wlricardo <wlricardo@gmail.com>
2026-08-12 21:15:35 -0300

    Criação da classe config.php

--- config.php
+++ config.php
@@ -17,3 +17,17 @@ spl_autoload_register(function ($nomeDaClasse) {
         }
     }
 });
+
+/**
+ *  Exemplo:
+ *  $nomeDoArquivo = $pastaAtual . $nomeDaClass . '.php'
+ *                 = classes/models/Animal.php 
+ *  Procurou pela classe Animal.php que está dentro da pasta 'models', que está dentro de 'classes'
+ */
+
+/**
+ *  Tha spl_autoload_function registers an anonymous function with PHP's SPL autoload stack. When PHP encounters 
+ *  an undefined class or interface, it calls registered autoloaders with the class name; here the parameter 
+ *  $nomeDaClasse will receive that fully qualified class name.
+ * 
+*/
